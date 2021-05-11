4c06e81cf992dea71c12715794f33bb748366e6a
alexandru-drincianu <alexdrincianu@gmail.com>
2021-05-11 09:55:07 +0300

    FITAPP-15 Create Main Page

--- src/main/java/fitness/application/controllers/MainPageController.java
+++ src/main/java/fitness/application/controllers/MainPageController.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -14,19 +15,29 @@ import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import fitness.application.services.*;
-import fitness.application.user.User;
-import java.io.IOException;
+import fitness.application.exceptions.*;
 
+import java.io.IOException;
 
 
 public class MainPageController {
 
     @FXML
-    Button exitButton;
+    Button exitButton,logOutButton;
+    @FXML
+    private Text welcomeText;
+    public void initialize() throws IOException {
+        welcomeText.setText("Welcome ");
+    }
     public void handleExit(MouseEvent mouseEvent) {
         Stage stage = (Stage) exitButton.getScene().getWindow();
         stage.close();
     }
-
+    public void handleLogOut(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/Login.fxml"));
+        Stage  window = (Stage)logOutButton.getScene().getWindow();
+        window.setTitle("Log In");
+        window.setScene(new Scene(root, 509,339));
+    }
 
 }
