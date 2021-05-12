5913c3afd87bbe574d0881793a40a411e2fb3d36
Paul-Carstean <carstean.paul@gmail.com>
2021-05-12 16:40:16 +0300

    FITAPP-6 As a trainer, I want to assign exercises to a customer

---
+++ src/main/java/fitness/application/controllers/AddExercisesController.java
@@ -0,0 +1,67 @@
+package fitness.application.controllers;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+import fitness.application.services.*;
+import fitness.application.exceptions.*;
+import fitness.application.user.*;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+import java.util.Objects;
+
+public class AddExercisesController {
+    @FXML
+    public Button backButton;
+    @FXML
+    public ChoiceBox customer,muscleGroup;
+    @FXML
+    private DatePicker dueDate;
+    @FXML
+    private Text messageText;
+    @FXML
+    private TextField exercisesNameField,setsField,seriesField;
+    @FXML
+    public void initialize() {
+        customer.getItems().addAll(UserServices.CustomerListUsername());
+        muscleGroup.getItems().addAll("Chest", "Back","Arms","Abdominals","Legs","Shoulders");
+    }
+
+
+    public void handleBack(javafx.scene.input.MouseEvent mouseEvent) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/CustomerListPage.fxml"));
+        Stage window = (Stage)backButton.getScene().getWindow();
+        window.setTitle("Customer List");
+        window.setScene(new Scene(root, 600,400));
+    }
+
+    public void handleSubmit(javafx.scene.input.MouseEvent mouseEvent) {
+        try {
+            LocalDate date = dueDate.getValue();
+            int year = 0;
+            int month = 0;
+            int day = 0;
+            if (!Objects.equals(date, null)) {
+                year = date.getYear();
+                month = date.getMonthValue();
+                day = date.getDayOfMonth();
+            }
+            UserServices.addExercise(UserServices.getLoggedInUsername(), (String)customer.getValue(), (String)muscleGroup.getValue(), exercisesNameField.getText(), setsField.getText(),seriesField.getText(),year, month, day);
+            messageText.setText("Execise was successfully added!");
+        } catch (emptyFieldException e) {
+            messageText.setText("You cannot leave empty text fields!");
+        }
+    }
+}
---
+++ src/main/java/fitness/application/user/Exercise.java
@@ -0,0 +1,100 @@
+package fitness.application.user;
+
+public class Exercise {
+
+    private String trainerName;
+    private String customerUsername;
+    private String muscleGroup;
+    private String exerciseName;
+    private String sets;
+    private String series;
+    private int year;
+    private int month;
+    private int day;
+
+    public Exercise(){}
+
+    public Exercise(String trainerName,String customerUsername,String muscleGroup,String exerciseName,String sets,String series, int year,int month,int day){
+        this.trainerName=trainerName;
+        this.customerUsername=customerUsername;
+        this.muscleGroup=muscleGroup;
+        this.exerciseName=exerciseName;
+        this.sets=sets;
+        this.series=series;
+        this.year=year;
+        this.month=month;
+        this.day=day;
+    }
+
+    public String getCustomerUsername() {
+        return customerUsername;
+    }
+
+    public void setCustomerUsername(String customerUsername) {
+        this.customerUsername = customerUsername;
+    }
+
+    public String getTrainerName() {
+        return trainerName;
+    }
+
+    public void setTrainerName(String trainerName) {
+        this.trainerName = trainerName;
+    }
+
+    public String getMuscleGroup() {
+        return muscleGroup;
+    }
+
+    public void setMuscleGroup(String muscleGroup) {
+        this.muscleGroup = muscleGroup;
+    }
+
+    public String getExerciseName() {
+        return exerciseName;
+    }
+
+    public void setExerciseName(String exerciseName) {
+        this.exerciseName = exerciseName;
+    }
+
+    public String getSets() {
+        return sets;
+    }
+
+    public void setSets(String sets) {
+        this.sets = sets;
+    }
+
+    public String getSeries() {
+        return series;
+    }
+
+    public void setSeries(String series) {
+        this.series = series;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getMonth() {
+        return month;
+    }
+
+    public void setMonth(int month) {
+        this.month = month;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+}
